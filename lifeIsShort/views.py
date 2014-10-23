07a69efff71bf90ceb7f1b51fd250cd5738e029f
xie-qianyue <xie.qianyue@gmail.com>
2014-10-23 15:46:29 +0200

    Modification of view, add the UI framework : Semantic UI

--- lifeIsShort/views.py
+++ lifeIsShort/views.py
@@ -5,6 +5,14 @@ from lifeIsShort.models import Activity
 def index(request):
 	    
 	activity_list = Activity.objects.all().filter(is_actif=True)
-	#import pdb;pdb.set_trace()	
-	context = {'activity_list' : activity_list}
+
+	activity_dict = {}
+        for activity in activity_list:
+                if activity.activity_type.type_name not in activity_dict:
+                        activity_dict[activity.activity_type.type_name] = list()
+                activity_dict[activity.activity_type.type_name].append(activity)
+	
+	# import pdb;pdb.set_trace()	
+	context = {'activity_dict' : activity_dict
+                   }
 	return render(request, 'lifeIsShort/index.html', context)
